698e9a0f057c06c5466bcd3503c9e81c6dee17db
Negev Volokita <41382878+negevvo@users.noreply.github.com>
2021-04-16 23:50:09 +0300

    Add files via upload

---
+++ ClearlyJS/clearly.js
@@ -0,0 +1,502 @@
+/**
+ * ClearlyJs
+ * @author Negev Volokita (negevvo)
+ * @version 1
+ * NOTE: Since private class members is new and not fully supported in JavaScript, they'll be public and start with _, but won't be documented
+ */
+class clrly {
+	// **************************** CLEARLYJS ****************************
+	/**
+	 * Imports a clearlyJs tool (LABS - may not work as expected)
+	 * @param {string} toolName Name of the tool to import, for example: "debug"
+	 * @returns HTML element of the tool's file
+	 */
+	static using(toolName){
+		var url = "http://localhost:16384/V2/clearlyJsV2.js";
+		url = url.replace(url.substring(url.lastIndexOf("/")), "");
+		switch(toolName.toLowerCase()){
+			case "debug":
+				url += "/clearlyDebug.js";
+				break;
+			case "fast":
+				url += "/clearlyFast.js";
+				break;
+			default:
+				console.error(new Error(`There is no ClearlyJs tool named ${toolName}`));
+		}
+		return this.import(url, "js", {defer: true});
+	}
+	// **************************** HTML ELEMENTS ****************************
+	/**
+	 * makes a new HTML element with a type, attributes and children
+	 * @param {string} type   Type of the element
+	 * @param {*} attributes  (optional) list of the element's attributes: {id: "hello", class: "someElement", parent: document.body, innerHTML: "Hello World"} etc
+	 * @param {HTML elements} children    (optional) children of the element
+	 * @returns an HTML element
+	 */
+    static new(type, attributes, ...children){
+		var newElement;
+		if(attributes == null){
+			attributes = "";
+		}
+		if (typeof(attributes) != "undefined"){
+			newElement = this._newElementWithAttributes(type, attributes);
+		}else{
+			newElement = this._newElement(type);
+		}
+		for (var i = 2; i < arguments.length; i++) {
+            var child = arguments[i];
+            newElement.appendChild(child.nodeType == null ? document.createTextNode(child.toString()) : child);
+        }
+		/**
+		 * Edits the element's attributes
+		 * @param {*} attributes attributes to change
+		 * @returns the element
+		 */
+		newElement.editAttributes = function(attributes){
+			return clrly.editAttributes(newElement, attributes);
+		}
+		return newElement;
+	}
+	static _newElementWithAttributes(type, attributes){
+		var newElement = document.createElement(type);
+		for(var attribute in attributes){
+			if(attribute != "parent" && attribute != "innerHTML"){
+				newElement.setAttribute(attribute, attributes[attribute]);
+			}
+		}
+		var parent = document.body;
+		if(attributes["parent"]){
+			parent = attributes["parent"];
+		}
+		if(attributes["innerHTML"]){
+			newElement.innerHTML = attributes["innerHTML"];
+		}
+		parent.appendChild(newElement);
+		return newElement;
+	}
+	static _newElement(type){
+		var newElement = document.createElement(type);
+		document.body.appendChild(newElement);
+		return newElement;
+	}
+
+	/**
+	 * Edits the given element's attributes
+	 * @param {*} element HTML element
+	 * @param {*} attributes attributes to change
+	 * @returns the element
+	 */
+	static editAttributes(element, attributes){
+		for(var attribute in attributes){
+			if(attribute != "parent" && attribute != "innerHTML"){
+				element.setAttribute(attribute, attributes[attribute]);
+			}
+			if(attributes["parent"]){
+				var parent = attributes["parent"];
+				parent.appendChild(element);
+			}
+			if(attributes["innerHTML"]){
+				element.innerHTML = attributes["innerHTML"];
+			}
+		}
+		return element;
+	}
+
+	/**
+	 * adds HTML code into document
+	 * @param {string} HTML HTML code to be added
+	 * @param {*} parent    (optional) parent element for the code to be inserted in
+	 * @returns             the HTML code
+	 */
+	static add(HTML, parent){
+		if(typeof(parent) != "undefined"){
+			parent.innerHTML+=HTML;
+		}else{
+			document.write(HTML);
+		}
+		return HTML;
+	}
+
+	/**
+	 * Get element by id
+	 * @param {string} id id of the element
+	 * @returns           DOM element with the specified id, or null
+	 */
+	static id(id){
+		try{
+		return document.getElementById(id);
+		}catch(e){
+			return null;
+		}
+	}
+
+	/**
+	 * Get elements by class name
+	 * @param {string} className class name of the elements
+	 * @returns                  HTML elements collection which includes all elements with the specified class name, or null
+	 */
+	static class(className){
+		try{
+			return document.getElementsByClassName(className);
+		}catch(e){
+			return null;
+		}
+	}
+
+	/**
+	 * Get elements by tag name
+	 * @param {string} tagName tag name of the elements
+	 * @returns                HTML elements collection which includes all elements with the specified tag name, or null
+	 */
+	static tag(tagName){
+		try{
+			return document.getElementsByTagName(tagName);
+		}catch(e){
+			return null;
+		}
+	}
+
+	/**
+	 * Get elements by attribute
+	 * @param {string} attribute attribute name of the elements
+	 * @param {string} value     value of the attribute
+	 * @returns                  HTML elements collection which includes all elements with the specified attribute and attribute value, or null
+	 */
+	 static attribute(attribute, value){
+		try{
+			return document.querySelectorAll('['+attribute+'="'+value+'"]');
+		}catch(e){
+			return null;
+		}
+	}
+
+	/**
+	 * Add CSS style
+	 * @param {string} style style to be added
+	 * @returns              the style element in the HTML
+	 */
+	static style(style){
+		return this.new("style", {parent: document.head, innerHTML: style});
+	}
+
+	/**
+	 * Import style / script
+	 * @param {string} link  link to import
+	 * @param {string} type  css / js
+	 * @param {*} attributes {defer: true} etc...
+	 * @returns				 HTML element of the imported file
+	 */
+	static import(link, type, attributes){
+		var element = null;
+		switch(type.toLowerCase()){
+			case "css":
+				element = this.new("link", {parent: document.head, href: link, rel: "stylesheet"});
+				break;
+			case "js":
+				element = this.new("script", {parent: document.head, src: link});
+				break;
+		}
+		for(var attribute in attributes){
+			element.setAttribute(attribute, attributes[attribute]);
+		}
+		return element;
+	}
+
+	/**
+	 * Set the document's title
+	 * @param {string} title the new title
+	 * @returns              the page's title
+	 */
+	static title(title){
+		Array.from(this.tag("title")).forEach(function(element){
+			element.remove();
+		});
+		this.new("title", {parent: document.head, innerHTML: title});
+		return this.tag("title")[0].innerHTML;
+	}
+
+	/**
+	 * Set the document's icon
+	 * @param {string} src URL of the new icon
+	 * @returns            the page's icon src
+	 */
+	static icon(src){
+		Array.from(this.attribute("rel", "icon")).forEach(function(element){
+			element.remove();
+		});
+		this.new("link", {parent: document.head, rel: "icon", href: src});
+		return this.attribute("rel", "icon")[0].href;
+	}
+
+	/**
+	 * Set the document's theme color
+	 * @param {string} color theme color (in hex)
+	 * @returns              the page's theme color
+	 */
+	 static theme(color){
+		Array.from(this.attribute("name", "theme-color")).concat(Array.from(this.attribute("name", "apple-mobile-web-app-status-bar-style"))).forEach(function(element){
+			element.remove();
+		});
+		this.new("meta", {parent: document.head, name: "theme-color", content: color});
+		this.new("meta", {parent: document.head, name: "apple-mobile-web-app-status-bar-style", content: color});
+		return this.attribute("name", "theme-color")[0].content;
+	}
+
+	/**
+	 * Change the dir attribute of the body element
+	 * @param {*} dir direction: RTL or LTR
+	 */
+	static dir(dir){
+		document.body.dir = dir;
+	}
+
+	/**
+	 * Initialize the app - set a title, icon, theme color, direction and mobile compatibility
+	 * @param {*} options {title: "title of the app", icon: "icon.png", theme: "#99ff99", dir: "RTL", mobile: true, using: "debug"}
+	 */
+	static initialize(options){
+		if(options["title"]){
+			this.title(options["title"]);
+		}
+		if(options["icon"]){
+			this.icon(options["icon"]);
+		}
+		if(options["theme"]){
+			this.theme(options["theme"]);
+		}
+		if(options["dir"]){
+			this.dir(options["dir"]);
+		}
+		if(options["using"]){
+			this.using(options["using"]);
+		}
+		if(options["mobile"] && options["mobile"] == true){
+		this.new("meta", {parent: document.head, name: "viewport", content: "width=device-width, initial-scale=1.0"});
+		}
+	}
+
+
+	// **************************** URL ****************************
+	/**
+	 * Current URL
+	 */
+	static get url(){
+		return window.location.href;
+	}
+	/**
+	 * The same URL, but without the search and hash elements
+	 */
+	static get pageUrl(){
+		return this.url.replace(window.location.search, "").replace(window.location.hash, "");
+	}
+	/**
+	 * URL hash (#)
+	 */
+	static get hash(){
+		return decodeURI(window.location.hash.substring(1));
+	}
+	/**
+	 * URL search (?) (AKA QueryString)
+	 * to get a value, use clrly.search.get('parameter')
+	 */
+	static get search(){
+		return new URL(location.href).searchParams;
+	}
+	/**
+	 * Reloads the page
+	 */
+	static reload(){
+		location.reload();
+	}
+	/**
+	 * Redirects to another URL
+	 * @param {string} URL URL to be redirected to
+	 */
+	static redirect(URL){
+		location.replace(URL);
+	}
+	/**
+	 * Go to another url
+	 * @param {string} URL        URL to go to
+	 * @param {boolean} newWindow (optional) open in a new window
+	 */
+	static go(URL, newWindow){
+		if(typeof(newWindow) != "undefined" && newWindow){
+			window.open(URL);
+		}else{
+			window.location.href = URL;
+		}
+	}
+	/**
+	 * Opens a new window with the same URL
+	 */
+	static duplicate(){
+		this.go(this.url, true);
+	}
+	/**
+	 * Goes to the same URL, but without the search and hash elements
+	 * @see pageUrl
+	 */
+	static pageURL(){
+		this.go(this.pageUrl);
+	}
+
+
+	// **************************** COOKIES ****************************
+	static get _MONTHS(){
+		return ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+	}
+	/**
+	 * Makes a new cookie
+	 * @param {string} name    name of the cookie
+	 * @param {string} content content of the cookie
+	 * @param {int} day        (optional) Expiration day
+	 * @param {int} month      (optional) Expiration month
+	 * @param {int} year       (optional) Expiration year
+	 * @returns content
+	 */
+	static cookie(name, content, day, month, year){
+		var date = "31 Dec 2036";
+		if(typeof(day) != "undefined" && typeof(month) != "undefined" && typeof(year) != "undefined"){
+			month = this._MONTHS[month-1];
+			date = (day) + ' ' + month + ' ' + year;
+		}
+		document.cookie = name+'='+content+'; expires=' + date + ' 23:59:59 UTC; path=/';
+		return content;
+	}
+	/**
+	 * Deletes a cookie
+	 * @param {string} cookieName cookie name to be deleted
+	 */
+	static deleteCookie(cookieName){
+		document.cookie = cookieName+'=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
+	}
+	/**
+	 * Gets the content of the cookie
+	 * @param {string} cookieName name of the cookie
+	 * @returns content of the cookie
+	 */
+	static getCookie(cookieName){
+		var name = cookieName + "=";
+    	var decodedCookie = decodeURIComponent(document.cookie);
+    	var ca = decodedCookie.split(';');
+    	for (var i = 0; i < ca.length; i++) {
+        	var c = ca[i];
+        	while (c.charAt(0) == ' ') {
+            	c = c.substring(1);
+        	}
+        	if (c.indexOf(name) == 0) {
+            	return c.substring(name.length, c.length);
+        	}
+    	}
+    	return "";
+	}
+
+
+	// **************************** XML ****************************
+	/**
+	 * Opens an xml file
+	 * @param {sttring} URL URL of the XML file
+	 * @param {function} callback a callback function that gets the xml
+	 */
+	static getXML(URL, callback) {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				callback(this.responseXML);
+			}
+		};
+		xmlhttp.open("GET", URL, true);
+		xmlhttp.send();
+	}
+
+
+	// **************************** XML ELEMENTS ****************************
+	/**
+	 * Get XML element by id
+	 * @param {*} xml XML element @see getXML
+	 * @param {string} id id of the XML element
+	 * @returns           XML element with the specified id, or null
+	 */
+	 static Xid(xml, id){
+		try{
+		return xml.getElementById(id);
+		}catch(e){
+			return null;
+		}
+	}
+
+	/**
+	 * Get XML elements by class name
+	 * @param {*} xml XML element @see getXML
+	 * @param {string} className class name of the XML elements
+	 * @returns                  all elements with the specified class name, or null
+	 */
+	static Xclass(xml, className){
+		try{
+			return xml.getElementsByClassName(className);
+		}catch(e){
+			return null;
+		}
+	}
+
+	/**
+	 * Get XML elements by tag name
+	 * @param {*} xml XML element @see getXML
+	 * @param {string} tagName tag name of the XML elements
+	 * @returns                all XML elements with the specified tag name, or null
+	 */
+	static Xtag(xml, tagName){
+		try{
+			return xml.getElementsByTagName(tagName);
+		}catch(e){
+			return null;
+		}
+	}
+
+	/**
+	 * Get XML elements by attribute
+	 * @param {*} xml XML element @see getXML
+	 * @param {string} attribute attribute name of the XML elements
+	 * @param {string} value     value of the attribute
+	 * @returns                  all XML elements with the specified attribute and attribute value, or null
+	 */
+	 static Xattribute(xml, attribute, value){
+		try{
+			return xml.querySelectorAll('['+attribute+'="'+value+'"]');
+		}catch(e){
+			return null;
+		}
+	}
+
+	/**
+	 * Get the value of an XML element
+	 * @param {*} element XML element
+	 * @returns the value of the xml element
+	 */
+	static Xvalue(XMLelement){
+		return XMLelement.firstChild.nodeValue;
+	}
+
+
+	// **************************** MATH ****************************
+	/**
+	 * Get a random float between two values (min < x < max when x is the random number)
+	 * @param {int} min the minimal number
+	 * @param {int} max the maximal number
+	 * @returns a random number between the two values
+	 */
+	 static random(min, max){
+		return Math.random() * (max - min) + min;
+	}
+	/**
+	 * Get a random integer between two values (min ≤ x ≤ max when x is the random number)
+	 * @param {int} min the minimal number
+	 * @param {int} max the maximal number
+	 * @returns a random number between the two values
+	 */
+	static randomInt(min, max){
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+}
---
+++ ClearlyJS/clearlyDebug.js
@@ -0,0 +1,134 @@
+/**
+ * ClearlyDebug - a debugging tool for ClearlyJs
+ * @author Negev Volokita (negevvo)
+ * @version 1
+ */
+class clrlyDebug{
+	/**
+	 * starts a debugging session
+	 */
+	static debug(){
+		clrly.style(`
+			#clearlyErrorButton, #clearlyErrorButtonBadge{
+				color: white !important;
+				position: fixed !important;
+				left: 15px !important;
+				bottom: 15px !important;
+				z-index: 1000 !important;
+				background-color: #ff4d4d !important;
+				filter: drop-shadow(0 0 8px #ff4d4d) !important;
+				height: 50px !important;
+				width: 50px !important;
+				border: 0 !important;
+				outline: none !important;
+				font-size: 30px !important;
+				transition: all 1s !important;
+				display: none;
+				border-radius: 10px !important;
+				font-family: sans-serif !important;
+			}
+			#clearlyErrorButton:hover{
+				filter: drop-shadow(0 0 20px #ff4d4d) !important;
+				cursor: pointer !important;
+			}
+			#clearlyErrorsback{
+				display: none;
+				position: fixed !important;
+				z-index: 1001 !important;
+				height: 100% !important;
+				width: 100% !important;
+				top: 0 !important;
+				bottom: 0 !important;
+				left: 0 !important;
+				right: 0 !important;
+				background-color: rgba(0, 0, 0, 0.3) !important;
+				backdrop-filter: blur(5px) !important;
+				border-radius: 0 !important;
+			}
+			#clearlyErrors{
+				margin: auto !important;
+				display: none;
+				position: fixed !important;
+				top: 50% !important;
+				left: 50% !important;
+				transform: translate(-50%, -50%) !important;
+				z-index: 1002 !important;
+				height: 80% !important;
+				width: 80% !important;
+				border-radius: 20px !important;
+				background-color: rgba(255, 255, 255, 0.7) !important;
+				backdrop-filter: blur(2px) !important;
+				-webkit-backdrop-filter: blur(2px) !important;
+				padding: 15px !important;
+				overflow: auto !important;
+				font-family: sans-serif !important;
+				text-align: left;
+			}
+			#clearlyErrors h1{
+				font-family: sans-serif !important;
+				font-size: 20px !important;
+				word-break: break-word !important;
+			}
+			#clearlyErrors a{
+				font-family: sans-serif !important;
+			}
+			#clearlyErrors a{
+				text-decoration: none !important;
+				color: #003399;
+			}
+		`);
+		var btn = clrly.new("button", {id: "clearlyErrorButton", innerHTML: "!", onclick: "clrlyDebug.showErrors()"});
+		clrly.new("div", {id: "clearlyErrorsBack", onclick: "clrlyDebug.hideErrors()"});
+		var div = clrly.new("div", {id: "clearlyErrors"});
+		window.onerror = function(message, url, line, column, error) {
+			var inFileTxt = "<br/>In file: ";
+			var lineTxt = "<br/>Line: ";
+			var columnTxt = ", Column: ";
+			var errorTxt = "<br/>";
+			switch(undefined || null || ""){
+				case message:
+					message = "";
+				case url:
+					url = "";
+					inFileTxt = "";
+				case line:
+					line = "";
+					lineTxt = "";
+				case column:
+					column = "";
+					columnTxt = "";
+				case error:
+					error = "";
+			}
+			clrly.new("h1", {parent: div, innerHTML: `${message}${inFileTxt}${url}${lineTxt}${line}${columnTxt}${column}${errorTxt}${error}`});
+			clrly.new("a", {parent: div, innerHTML: "Search error on Google", href: `https://www.google.com/search?q=${message}`, target: "_blank"});
+			clrly.new("br", {parent: div});
+			clrly.new("a", {parent: div, innerHTML: "Search error on Bing", href: `https://www.bing.com/search?q=${message}`, target: "_blank"});
+			clrly.new("br", {parent: div});
+			clrly.new("br", {parent: div});
+			btn.style.display = "unset";
+		};
+		var error = console.error;
+		console.error = function(message){
+			window.onerror(message);
+			error.apply(console, arguments);
+		};
+	}
+	/**
+	 * Show the Errors
+	 */
+	static showErrors(){
+		clrly.id("clearlyErrors").style.display = "unset";
+		clrly.id("clearlyErrorsBack").style.display = "unset";
+	}
+	/**
+	 * Hide the Errors
+	 */
+	static hideErrors(){
+		clrly.id("clearlyErrors").style.display = "none";
+		clrly.id("clearlyErrorsBack").style.display = "none";
+	}
+}
+
+//Starts debugging when file loads
+clrlyDebug.debug();
