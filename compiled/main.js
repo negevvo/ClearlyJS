69120316108fe2f1fad158c30c4de68d1a7f3af0
Negev Volokita <41382878+negevvo@users.noreply.github.com>
2022-04-21 23:07:29 +0300

    Add files via upload

--- compiled/main.js
+++ compiled/main.js
@@ -1,3 +1,4 @@
+import clrly from 'https://cdn.jsdelivr.net/npm/clearlyjs';
 init("Home", ".");
 mainStyle();
 var bg, bgTxt;
@@ -8,18 +9,14 @@ clrly.new("main", null, bg = clrly.new("div", {
 })), clrly.new("div", {
   class: "center"
 }, clrly.new("img", {
-  src: `${maindir}/icon.png`,
-  style: "height: 100px; filter: drop-shadow(0 0px 8px #2d2e1536);"
+  src: `${maindir}/logo.png`,
+  style: "width: 50vw;"
 }), clrly.new("h1", {
   class: "title",
   style: "width: 100vw;"
 }, generateTitle()), clrly.new("h2", {
-  class: "title"
-}, APP_NAME, clrly.new("br", null), "a JavaScript library by ", clrly.new("a", {
-  href: "https://twitter.com/negevvo",
-  target: "_blank"
-}, "negevvo"), clrly.new("br", null), "(Stable, pre-release 1)"), clrly.new("h2", {
-  class: "title"
+  class: "title",
+  style: "cursor: default;"
 }, clrly.new("a", {
   href: "https://negevvo.gitbook.io/clearlyjs/getting-started/get-started-with-clearlyjs"
 }, "Get started!"), " | ", clrly.new("a", {
@@ -28,7 +25,8 @@ clrly.new("main", null, bg = clrly.new("div", {
   href: "https://github.com/negevvo/ClearlyJS",
   target: "_blank"
 }, "Github")), clrly.new("h2", {
-  class: "title"
+  class: "title",
+  style: "cursor: default;"
 }, "Start in: ", clrly.new("a", {
   target: "_blank",
   href: "https://codesandbox.io/s/clearlyjs-template-wrsjv?fontsize=14&hidenavigation=1&module=%2Fcode%2Findex.js&theme=dark"
@@ -36,7 +34,7 @@ clrly.new("main", null, bg = clrly.new("div", {
   target: "_blank",
   href: "https://codepen.io/pen/?template=KKajRxM"
 }, "CodePen"))));
-const FEATURES = [`${APP_NAME}`, `Simple`, `Dynamic`, `Light`, `Fast`, `JSX support`, `The new JavaScript`, `Debugging tools`, `Super Fast image loading`, `AJAX requests`, `XML tools`, `DOM tools`, `URL tools`, `Cookies`, `Components`, `Vanilla JS support`, `Mobile compatible`, `Open source`];
+const FEATURES = [`${APP_NAME}`, `Simple`, `Dynamic`, `Light`, `Fast`, `JSX support`, `The new JavaScript`, `Debugging tools`, `Super Fast image loading`, `AJAX requests`, `XML tools`, `DOM tools`, `URL tools`, `Cookies`, `Blobs`, `Components`, `Vanilla JS support`, `Mobile compatible`, `Open source`];
 
 for (var i = 0; i < 10; i++) {
   FEATURES.forEach(function (feature, index) {
@@ -49,7 +47,23 @@ for (var i = 0; i < 10; i++) {
 }
 
 function generateTitle() {
-  const TITLE = "Make it simple";
+  const TITLE = "Welcome!";
   const TITLES = [`clrly.new("h1", {innerHTML: "${TITLE}"});`, `clrly.new("h1", {}, "${TITLE}");`, `<h1>${TITLE}</h1>`];
   return TITLES[clrly.randomInt(0, TITLES.length - 1)];
+}
+
+console.log(`Hey! What's your name?\nRespond with: myNameIs("name")`);
+
+function myNameIs(yourName, download) {
+  var HTML = `
+    <title>Hi!</title>
+    <h1>Hello ${yourName}, Nice to meet you!</h1>
+    <h2>Hope your'e enjoying ${APP_NAME}!<br/>Remember to leave some feedback on Github!</h2>
+    `;
+
+  if (download) {
+    clrly.download(clrly.toFile(HTML), "Hi.html");
+  } else {
+    clrly.go(clrly.toFile(HTML), true);
+  }
 }
